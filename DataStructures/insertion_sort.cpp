95b002cc8280747715648d535df577cebd96b174
geniusfhr <geniusfhr@163.com>
2015-12-21 15:54:30 +0800

    testTerminal

--- DataStructures/insertion_sort.cpp
+++ DataStructures/insertion_sort.cpp
@@ -1,37 +1,38 @@
 /*
+*insetion_sort.cpp
 *插入排序
 *
+*有序段不断增加 无序段元素不断减少的过程
 */
 
-
 #include <stdio.h>
-#define MAX 100
-int a[MAX];
 
 
-
-void insertion_sort(int* a,int n ){
-    for(int i = 2;i <= n;i++ ){
-        int key = a[i];
-        int j;
-        for(j = 1;j <= i-1; j++){
-            if(key <= a[j]){
-                break;
+void insertion_sort(int* a,int n ){       //直接插入排序 从小到大
+    for(int i = 2; i <= n; i++){
+        a[0] = a[i];                     //哨兵 记录插入元素
+        if(a[i] < a[i-1]){              //与有序端最后比较  需要插入
+            int j;
+            for( j = 0; j <= i-1; j++){
+                if(a[0] < a[j])          //找到插入的位置 j 跳出循环
+                    break;
             }
+
+            for(int k = i-1; k >= j; k--){      // j 到 i-1 后移一位
+                a[k+1] = a[k];
+            }
+            a[j] = a[0];                       //插入该元素
         }
-        for(int k = i-1; k >= j;k--){
-            a[k+1] = a[k];
-        }
-        a[j] = key;
     }
 }
 
 
 int main(){
-    int a[10] = {0,5,4,3,2,1};
+    int a[10] = {0,1,2,3,4,5};
     insertion_sort(a,5);
     for(int i = 1;i<=5;i++){
-        printf("%d",a[i]);
+        printf("%d ",a[i]);
     }
     return 0;
+
 }
