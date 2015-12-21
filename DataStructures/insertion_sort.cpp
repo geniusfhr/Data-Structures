146377c82e7e9fcbc6b74455d58538e5010c0d2c
geniusfhr <geniusfhr@163.com>
2015-12-21 22:09:17 +0800

    test

--- DataStructures/insertion_sort.cpp
+++ DataStructures/insertion_sort.cpp
@@ -7,13 +7,13 @@
 
 #include <stdio.h>
 
-
-void insertion_sort(int* a,int n ){       //直接插入排序 从小到大
+//先找位置 再移动插入
+void my_insertion_sort(int* a,int n ){       //直接插入排序 从小到大
     for(int i = 2; i <= n; i++){
         a[0] = a[i];                     //哨兵 记录插入元素
         if(a[i] < a[i-1]){              //与有序端最后比较  需要插入
-            int j;
-            for( j = 1; j <= i-1; j++){
+             int j;
+             for( j = 1; j <= i-1; j++){
                 if(a[0] <= a[j])          //找到插入的位置 j 跳出循环
                     break;
             }
@@ -27,9 +27,26 @@ void insertion_sort(int* a,int n ){       //直接插入排序 从小到大
 }
 
 
+//从小到大 直接插入排序
+//比较 移动同时进行
+
+void std_insertion_sort(int* a,int n ){
+    for(int i = 2;i <= n; i++){      //从 2 到  n
+        if(a[i] < a[i-1]){          //有必要插入
+             a[0] = a[i];           //设立哨兵 需要插入的元素
+             a[i] = a[i-1];
+             int j;
+             for(j = i-2; a[0] <= a[j] && j>0 ;j--){
+                 a[j+1] = a[j];        //后移
+             }
+             a[j+1] = a[0] ;          //插入元素
+        }
+    }
+}
+
 int main(){
     int a[10] = {0,1,2,3,4,5};
-    insertion_sort(a,5);
+    std_insertion_sort(a,5);
     for(int i = 1;i<=5;i++){
         printf("%d ",a[i]);
     }
