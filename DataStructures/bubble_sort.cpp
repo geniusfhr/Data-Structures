0e2493cb9a634edb804d4db1035da2abff740c6d
geniusfhr <geniusfhr@163.com>
2015-12-24 21:32:13 +0800

    bubble_sort

---
+++ DataStructures/bubble_sort.cpp
@@ -0,0 +1,46 @@
+/*
+*
+*bubble_sort.cpp
+*冒泡排序
+*
+*/
+
+#include <stdio.h>
+
+int compare(int a,int b){
+    if(a >= b)
+        return 1;
+    else
+        return 0;
+}
+
+
+
+//exchange function
+void exchange(int& a,int& b){
+    int t = a;
+    a = b;
+    b = t;
+}
+
+//bubble sort func
+void bubble_sort(int* array,n){
+    for(int i = n-1; i >= 1; i--){
+        for(int j = 1;j <= i;j++){
+            if(compare(array[j],array[j+1])){
+                exchange(array[j],array[j+1]);
+            }
+        }
+    }
+}
+
+
+int main(){
+    int a[] = {0,5,4,3,2,1}
+    bubble_sort(a,5);
+    for(int i = 1; i <= 5; i++){
+        printf("%d ",a[i]);
+    }
+
+    return 0;
+}
